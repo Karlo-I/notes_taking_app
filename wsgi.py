d48864e7d1eabcb42677e898c0e9ba9107dcee13
karloigloria <karloigloria@hotmail.co.uk>
2026-09-13 19:00:34 +0100

    navbar updates

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,7 @@ from app_factory import create_app  # noqa: E402 -- must follow load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
     port = int(os.environ.get('PORT', 5002))
     app.run(host='0.0.0.0', port=port, debug=False)
